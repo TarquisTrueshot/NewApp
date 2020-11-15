91dd9e0468d5519a29cb3188988072b41703f5ee
TarquisTrueshot <kingssword2k3@yahoo.com>
2020-11-15 00:16:08 -0500

    Add project files.

---
+++ NewApp/NewApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NewApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0; // declare the count variable.
+
+        // This event is called when the click me button is clicked.
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times!";
+        }
+
+        // This event is called when the reset button is clicked.
+        void Handle_ResetClicked(object sender, System.EventArgs e)
+        {
+            count = 0;
+            clickMeBtn.Text = $"Click Me!";
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
